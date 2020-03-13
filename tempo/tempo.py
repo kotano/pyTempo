8dc557e8166907a0637cceab061abb564e6944a0
kotano <kot4no@gmail.com>
2020-03-13 13:37:14 +0300

    Dropdown added

--- tempo/tempo.py
+++ tempo/tempo.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.graphics.instructions import Canvas
+from kivy.properties import ListProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.layout import Layout
@@ -9,14 +11,29 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 
-class ListWindow(GridLayout):
+class Task(BoxLayout):
     pass
 
 
-class TempoApp(App):
+class PriorityDropdown(BoxLayout):
 
+    # def dropdown_open(self, button):
+
+    pass
+
+
+class RootWidget(BoxLayout):
+    taskholder = ObjectProperty()
+    task_ids = ListProperty([])
+
+    def add_list_item(self):
+        task = Task()
+        self.taskholder.add_widget(task)
+
+
+class TempoApp(App):
     def build(self):
-        pass
+        return RootWidget()
 
 
 if __name__ == '__main__':
