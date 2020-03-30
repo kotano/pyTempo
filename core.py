7a67f66e2b5150ed8a0a6c429df05ab511331c88
kotano <kot4no@gmail.com>
2020-03-30 16:30:00 +0300

    Added progress bar

---
+++ core.py
@@ -0,0 +1,9 @@
+import datetime
+import time
+
+
+print(time.gmtime(0))
+
+
+if __name__ == "__main__":
+    pass
--- tempo/tempo.py
+++ tempo/tempo.py
@@ -22,6 +22,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.uix.rst import RstDocument
+from kivy.uix.bubble import Bubble
 
 DATAFILE = os.path.join(os.path.dirname(__file__), 'data.json')
 
@@ -29,14 +30,15 @@ DATAFILE = os.path.join(os.path.dirname(__file__), 'data.json')
 TASKDATA = ('''
 Task:
     id: task
-    # taskid: task.__self__
     taskname: taskname.__self__
     priority: priority.__self__
     time: time.__self__
     deadline: deadline.__self__
     checkbox: checkbox.__self__
+    
     popup: popup.__self__
     notes: notes.__self__
+    
 
     # on_parent: app.root.save_tasks(self.taskname.text, self.priority.text, self.time.text, self.deadline.text)
     
@@ -48,49 +50,88 @@ Task:
         width: 20
         pos: root.center
     
-    # TASKNAME
+# TASKNAME # POPUP
     BoxLayout:
         size_hint_x: 2
         id: bl
+
         Popup:
             id: popup
-            title: "Editing task"
+            title: "Edit task"
+            # background_color: 1, 1, 1, 1
             on_parent:
                 if self.parent == bl: self.parent.remove_widget(self)
             BoxLayout:
+                canvas.before:
+                    Color:
+                        rgba: 1, 1, 1, 1
+                    Rectangle:
+                        pos: self.pos
+                        size: self.size
+                background_color: 1, 1, 1, 1
                 orientation: 'vertical'
 
-                TextInput:
-                    id: taskname
-                    text: '{taskname}'
-                    write_tab: False
-                    focus: True
-                    hint_text: 'Enter task name'
-                    multiline: False
-                    size_hint: 1, 0.3
+                BoxLayout:
+                    size_hint: 1, None
+                    height: 50
+                    spacing: 40, 40
+
+                    PrioritySpinner:
+                        text: priority.text
+                        size_hint: 0.1, 1
+
+                    TextInput:
+                        id: taskname
+                        text: '{taskname}'
+                        write_tab: False
+                        focus: True
+                        hint_text: 'Enter task name'
+                        multiline: False
+                        size_hint: 0.8, 1
 
 
-                Label:
-                    text: 'Add notes to the task'
+                BoxLayout:
+                    padding: 10, 10
+                    size_hint_y: None
+                    height: 50
+
+                    TextInput:
+                        size_hint_x: 0.25
+                        text: 'xx/xx/xxxx'
+                    Button:
+                        size_hint_x: 0.5
+                        text: '23 HOURS OF 30'
+                    TextInput:
+                        size_hint_x: 0.25
+                        text: 'xx/xx/xxxx'
+                        
+                
+                
                 TextInput:
                     id: notes
                     text: '{notes}'
                     hint_text: 'Notes...'
-                    size_hint: 1, 2
+                    size_hint: 1, 1
+                    height: 100
                 BoxLayout:
+                    size_hint_y: None
+                    height: 50
+                    
                     Button:
-                        text: 'press to dismiss'
+                        text: 'Save'
                         on_release: 
                             popup.dismiss();
                             app.root.save_tasks();
                     Button:
-                        text: 'Delete task'
-                        size_hint_x: 0.3
+                        text: 'Delete'
+                        size_hint_x: None
+                        width: 50
                         background_color: 1, 0, 0, 1
                         on_release:
                             app.root.taskholder.remove_widget(task);
                             popup.dismiss();
                             app.root.save_tasks();
+
         Button:
             background_normal: ''
             color: 0, 0, 0, 1
