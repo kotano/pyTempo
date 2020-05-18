40731898cc182699145433cc280a6ef1457a8bd8
eliz0106 <58878965+eliz0106@users.noreply.github.com>
2020-05-18 23:24:12 +0300

    tests

--- tempo/tests/test_dates.py
+++ tempo/tests/test_dates.py
@@ -12,3 +12,12 @@ def test_find_deltatime():
 def test_find_worktime():
     # If we have one day, so then we have only HOURSPERDAY hours value
     assert dates.HOURSPERDAY == dates.find_worktime(24)
+
+
+def test_date_to_string():
+    date = datetime.date(2020, 6, 22)
+    assert '22.06.2020' == dates.date_to_string(date)
+
+def test_convert_to_date():
+    s = '22.06.2020'
+    assert datetime.date(2020, 6, 22) == dates.convert_to_date(s)
